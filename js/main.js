9aceb7f85959ae897e7a764bb69a64101d3f19ff
rianstroe <79443296+rianstroe@users.noreply.github.com>
2021-02-22 11:03:03 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('a[href^="#"]').click(function (){
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated), body:not(:animated)").animate({scrollTop: destination}, 800);
+        return false;
+    })   
+$('.reviews').slick({
+  dots: false,
+  infinite: true,
+  speed: 500,
+  fade: false,
+  cssEase: 'linear'
+});   
+
+var opts = {
+    countdown: true,
+    language: "it-it"
+}
+
+$('.timer').FlipClock(1510, opts);
+
+$('.timer2').FlipClock(1510, opts);
+   
+});
